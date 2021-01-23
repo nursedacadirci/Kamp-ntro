73c01b804a832d74ddd1eda9ae5de402a0524fde
seda çadırcı <sedaar869@gmail.com>
2021-01-23 19:32:06 +0300

    MY DİCTİONARY

---
+++ MyDictionary1/MyList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary1
+{
+    class MyList<T>
+    {
+        T[] items;
+
+        //constructor
+        //ctor tab tab
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+
+        public void Add(T item)
+        {
+            //önceki eklenen elemanları tutar
+            T[] tempArray = items;
+
+
+            //eleman sayısı bir arttırılır
+            items = new T[items.Length + 1];
+
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+
+
+
+
+        }
+    }
+}
---
+++ MyDictionary1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyDictionary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+
+            isimler.Add("NURSEDA ÇADIRCI");
+        }
+    }
+}
